3329bb9b5a2bf0ce0b9de4f9caf1df4b8683773b
MDfarazuddin99 <52372209+MDfarazuddin99@users.noreply.github.com>
2024-05-23 18:00:03 -0700

    style adjustments for prepper homepage

--- src/components/prepper/prepper-bottomNavBar.tsx
+++ src/components/prepper/prepper-bottomNavBar.tsx
@@ -1,52 +1,48 @@
 "use client";
-import { Clock, User, Star, LayoutGrid, ArrowUpDown, GalleryVerticalEnd } from "lucide-react";
-
+import {
+  Clock,
+  User,
+  Star,
+  LayoutGrid,
+  ArrowUpDown,
+  GalleryVerticalEnd,
+  List,
+} from "lucide-react";
 export function BottomNav() {
   return (
-    <div className="w-[50%] flex items-center justify-between p-4">
-      <div className="flex items-center space-x-4">
-        <button className="flex items-center space-x-1 text-gray-700">
-        <GalleryVerticalEnd size={20} className="shrink w-6 h-6" />
-          <span>All</span>
+    <div className="w-[80%] flex items-center justify-between py-4 px-7">
+      <div className="flex items-center space-x-6">
+        <button className="flex items-center space-x-1 text-black-700">
+          <GalleryVerticalEnd size={20} className="shrink w-6 h-6" />
+          <span className="font-bold">All</span>
         </button>
         <button className="flex items-center space-x-1 text-purple-700 bg-purple-100 px-3 py-1 rounded">
-        <Clock size={20} className="shrink w-6 h-6" />
-          <span>Recently viewed</span>
+          <Clock size={20} className="shrink w-6 h-6" />
+          <span className="font-bold">Recently viewed</span>
         </button>
-        <button className="flex items-center space-x-1 text-gray-700">
-        <User size={20} className="shrink w-6 h-6" />
-          <span>Created by you</span>
+        <button className="flex items-center space-x-1 text-black-700">
+          <User size={20} className="shrink w-6 h-6" />
+          <span className="font-bold">Created by you</span>
         </button>
-        <button className="flex items-center space-x-1 text-gray-700">
-        <Star size={20} className="shrink w-6 h-6"/>
-          <span>Favorites</span>
+        <button className="flex items-center space-x-1 text-black-700">
+          <Star size={20} className="shrink w-6 h-6" />
+          <span className="font-bold">Favorites</span>
         </button>
       </div>
-      <div className="flex items-center space-x-2">
-        <button className="flex items-center text-gray-700">
-        <ArrowUpDown size={20} className="shrink w-6 h-6"/>
+      <div className="flex items-center space-x-2 p-3">
+        <button className="flex items-center text-black-700 mr-2">
+          <ArrowUpDown size={20} className="shrink w-6 h-6" />
         </button>
-        <button className="flex items-center text-purple-700 bg-purple-100 px-3 py-1 rounded">
-        <LayoutGrid size={20} className="shrink w-6 h-6" />
-          <span className="ml-1">Grid</span>
-        </button>
-        <button className="flex items-center text-gray-700">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 10h16M4 14h16M4 18h16"
-            />
-          </svg>
-          <span className="ml-1">List</span>
-        </button>
+        <div className="flex bg-gray-300 py-1 px-2 rounded">
+          <button className="flex items-center text-purple-700 px-3 py-1 rounded bg-white">
+            <LayoutGrid size={20} className="shrink w-6 h-6" />
+            <span className="font-bold ml-1">Grid</span>
+          </button>
+          <button className="flex items-center text-black-700 ml-2">
+            <List size={20} className="shrink w-6 h-6" />
+            <span className="font-bold ml-1">List</span>
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/prepper/prepper-topNavBar.tsx
+++ src/components/prepper/prepper-topNavBar.tsx
@@ -8,7 +8,7 @@ import {
 
 export function TopNav() {
   return (
-    <div className="w-[50%] inline-block items-center justify-between p-4">
+    <div className="w-[80%] inline-block items-center justify-between p-4">
       <div className="flex pb-2 items-center space-x-2">
         <div className="flex items-center space-x-1">
           <GalleryVerticalEnd size={20} className="shrink w-6 h-6" />
@@ -19,20 +19,20 @@ export function TopNav() {
         <button className="bg-purple-600 text-white px-4 py-2 rounded flex items-center space-x-1">
           <Plus size={20} className="shrink w-6 h-6" />
 
-          <span>Create new</span>
+          <span className="font-bold">Create new</span>
           <span className="bg-purple-700 text-xs px-3 py-1 rounded ">AI</span>
         </button>
         <div className="relative">
           <button className="bg-white text-black-700 px-4 py-2 rounded border border-gray-300 flex items-center">
             <Plus size={20} className="shrink w-6 h-6" />
-            <span>New from blank </span>
+            <span className="font-bold">New from blank </span>
           </button>
           {/* Dropdown can be implemented here */}
         </div>
         <div className="relative">
           <button className="bg-white text-black-700 px-4 py-2 rounded border border-gray-300 flex items-center">
             <FolderInput size={20} className="shrink w-6 h-6 mr-2" />
-            <span>Import</span>
+            <span className="font-bold">Import</span>
             <ChevronDown size={20} className="shrink w-6 h-6 ml-2" />
           </button>
           {/* Dropdown can be implemented here */}
